3f37ebb418d9c10c0fad03e213dfd40927ede0ae
janpro <janpro0706@gmail.com>
2016-09-26 23:08:39 +0900

    1-6: implemented wall to fade out in few seconds

--- Assets/WallScript.cs
+++ Assets/WallScript.cs
@@ -2,15 +2,34 @@
 using System.Collections;
 
 public class WallScript : MonoBehaviour {
+    private IEnumerator fadeOut;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+        fadeOut = FadeOutIn(5000);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (fadeOut.MoveNext() != false)
+        {
+            MoveLeft();
+        }
+    }
+
+    void MoveLeft()
+    {
         var pos = transform.position;
-        transform.position = new Vector3(pos.x - 0.1f, pos.y, pos.z);
-	}
+        transform.position = new Vector3(pos.x - 0.01f, pos.y, pos.z);
+    }
+    
+    IEnumerator FadeOutIn(long millis)
+    {
+        float countdown = millis / 1000.0f;
+
+        while ((countdown -= Time.deltaTime) > 0) yield return false;
+
+        gameObject.SetActive(false);
+        yield return true;
+    }
 }
