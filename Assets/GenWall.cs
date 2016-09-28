6d4aad18aeb8006fdb596500f4ac2b684d063cd4
janpro <janpro0706@gmail.com>
2016-09-28 21:57:36 +0900

    1-9: instantiate wall at random y position

--- Assets/GenWall.cs
+++ Assets/GenWall.cs
@@ -13,7 +13,9 @@ public class GenWall : MonoBehaviour {
         camHeight = cam.orthographicSize * 2;
         camWidth = camHeight * cam.aspect;
 
-        Instantiate(wall, new Vector3(8.5f, 0, 0), Quaternion.identity);
+        //float[] p = RandomGenPosition();
+        //GenerateWall(p[0], p[1]);
+        GenerateWall(Random.Range(-10.0f, 10.0f));
         StartCoroutine(GenerateInterval(5));
 	}
 	
@@ -32,11 +34,50 @@ public class GenWall : MonoBehaviour {
             else
             {
                 // Generate Wall from prefab
-                Instantiate(wall, new Vector3(camWidth / 2, 0, 0), Quaternion.identity);
+                //float[] p = RandomGenPosition();
+                //GenerateWall(p[0], p[1]);
+                GenerateWall(Random.Range(-10.0f, 10.0f));
 
                 count += sec;
                 yield return true;
             }
         }
+    }
+
+    // scale 문제 때문에 제대로 동작하지 않음
+    void GenerateWall(float bottom, float top)
+    {
+        float height = top - bottom;
+        float y = bottom + height / 2.0f;
+        float scaleY = height / 10.0f / 2.0f;
+
+        Object instance = Instantiate(wall, new Vector3(camWidth / 2, y, 0), Quaternion.identity);
+    }
+
+    void GenerateWall(float y)
+    {
+        Instantiate(wall, new Vector3(camWidth / 2, y, 0), Quaternion.identity);
+    }
+
+    float[] RandomGenPosition()
+    {
+        float b = 0, t = 0;
+
+        while (Mathf.Abs(t - b) < 3)
+        {
+            b = Random.Range(-5.0f, 5.0f);
+            t = Random.Range(-5.0f, 5.0f);
+        }
+
+        if (b > t)
+        {
+            float temp = b;
+            b = t;
+            t = temp;
+        }
+
+        float[] pos = { b, t };
+
+        return pos;
     }
 }
